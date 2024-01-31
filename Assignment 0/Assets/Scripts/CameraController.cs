53b1dce6fffd0a30d70fbdff954d5a108ef726cf
Daniel Garza <danielrgarza12@gmail.com>
2024-01-30 19:38:37 -0600

    add player movement

---
+++ Assignment 0/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player; //ref to game object
+    private Vector3 offset; //distance from camera and player
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //calculate initial offset between cameras position
+        offset = transform.position - player.transform.position;
+    }
+
+    // alled once per frame after all ppdate functions have been completed
+    private void LateUpdate()
+    {
+        // maintain the same offset between camera and player
+        transform.position = player.transform.position + offset;
+    }
+
+}
--- Assignment 0/Assets/Scripts/PlayerController.cs
+++ Assignment 0/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,12 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
+    //speed player is moving
+    public float speed = 5;
 
     private Rigidbody rb;
-    private float movementX;
-    private float movementY;
+    private float movementX; //move along x axis
+    private float movementY; //move along y axis
 
     // Start is called before the first frame update
     void Start()
@@ -17,18 +18,23 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+    // function called when move input is detected
     void OnMove(InputValue movementValue)
     {
+        // convert input value into a vector2 movement
         Vector2 movementVector = movementValue.Get<Vector2>();
 
+        // storing x and y compenents of movement
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
-
+     // called one per fixed frame rate frame
     void FixedUpdate()
     {
+        // create 3D movement vector using x and y input
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
+        // apply force to rigid body to move the player
         rb.AddForce(movement * speed);
     }
 }
